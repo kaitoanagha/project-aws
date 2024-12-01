2c5ed08f380bdfea65e336234b458c3cb7e9ec9b
kaitoanagha <151990821+kaitoanagha@users.noreply.github.com>
2024-12-01 14:31:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Informasi Pribadi</title>
+</head>
+<body>
+    <h1>Informasi Mahasiswa</h1>
+    <?php
+        
+        $nama = "I Wayan Pasek Anagha Vinaya";
+        $nim = 23083000009;
+        $fakultas = "Teknologi Informasi";
+        $universitas = "Merdeka Malang";
+
+       
+        echo "<p>Nama: $nama</p>";
+        echo "<p>NIM: $nim</p>";
+        echo "<p>Fakultas: $fakultas</p>";
+        echo "<p>Universitas: $universitas</p>";
+    ?>
+</body>
+</html>
